8bcf752f13b52b367b0d83d334821147aecb0933
Moises Núñez Ibarra <mizulegendsgg@gmail.com>
2025-09-04 02:56:58 +0000

    Update loading.js

--- src/js/dev/loading.js
+++ src/js/dev/loading.js
@@ -1,3 +1,9 @@
-    document.addEventListener('DOMContentLoaded', (event) => {
-      document.body.style.opacity = '1';
-    });
+/**
+ * Module for managing page loading and preventing FOUC (Flash of Unstyled Content).
+ * @module Loading
+ */
+export function initializeLoadingScreen() {
+  document.addEventListener('DOMContentLoaded', (event) => {
+    document.body.style.opacity = '1';
+  });
+}
