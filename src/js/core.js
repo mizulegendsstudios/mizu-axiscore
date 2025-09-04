4fba52106015038ce0f43458e8cef320f5d2b137
Moises Núñez Ibarra <mizulegendsgg@gmail.com>
2025-09-04 03:36:52 +0000

    Update core.js

--- src/js/core.js
+++ src/js/core.js
@@ -2,7 +2,7 @@
  * @fileoverview Archivo principal que orquesta la lógica de la aplicación.
  * @author Gemini
  */
-import { initializeLoadingScreen } from "./dev/loading.js";
+import { initializeLoadingScreen } from "./stable/loading.js";
 import { initializeBarHiding } from "./stable/monitor_bars.js";
 import { initializeMonitor } from "./stable/monitor_axis.js";
 
@@ -10,7 +10,7 @@ import { initializeMonitor } from "./stable/monitor_axis.js";
  * Versión de la aplicación.
  * @constant {string}
  */
-const VERSION = '4.20.6';
+const VERSION = '4.20.7';
 
 document.addEventListener('DOMContentLoaded', () => {
     // Registra la versión de la aplicación en la consola.
