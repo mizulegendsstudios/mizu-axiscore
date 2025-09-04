7eb2a339e292e2cbad5fbc40817067af14791b85
Moises Núñez Ibarra <mizulegendsgg@gmail.com>
2025-09-04 03:43:29 +0000

    Update core.js

--- src/js/core.js
+++ src/js/core.js
@@ -13,7 +13,7 @@ import { initializeZoomAndPan } from "./dev/zoom.js";
  * Versión de la aplicación.
  * @constant {string}
  */
-const VERSION = '4.20.7';
+const VERSION = '4.20.8';
 
 document.addEventListener('DOMContentLoaded', () => {
     // Registra la versión de la aplicación en la consola.
