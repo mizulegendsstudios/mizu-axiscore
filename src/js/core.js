1b06fe66d76e969d7291bfdb40cb89c1c7f48e49
Moises Núñez Ibarra <mizulegendsgg@gmail.com>
2025-09-04 03:06:26 +0000

    Update core.js

--- src/js/core.js
+++ src/js/core.js
@@ -1,10 +1,10 @@
 import { initializeLoadingScreen } from "./dev/loading.js";
 import { initializeBarHiding } from "./stable/monitor_bars.js";
-import { updateMonitor } from "./stable/monitor_axis.js";
+import { initializeMonitor } from "./stable/monitor_axis.js";
 
 document.addEventListener('DOMContentLoaded', () => {
     // Inicializa el monitor de mouse y el tamaño del viewport
-    updateMonitor();
+    initializeMonitor();
 
     // Inicializa la lógica para ocultar y mostrar las barras
     initializeBarHiding();
