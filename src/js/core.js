802f8aca7b094742c468d0fc74886d30aacacc5c
Moises Núñez Ibarra <mizulegendsgg@gmail.com>
2025-09-04 03:11:25 +0000

    Update core.js

--- src/js/core.js
+++ src/js/core.js
@@ -6,7 +6,8 @@ import { initializeLoadingScreen } from "./dev/loading.js";
 import { initializeBarHiding } from "./stable/monitor_bars.js";
 import { initializeMonitor } from "./stable/monitor_axis.js";
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => 
+    {
     // Inicializa el monitor de mouse y el tamaño del viewport
     initializeMonitor();
 
@@ -15,15 +16,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Muestra la pantalla una vez que el DOM esté listo
     initializeLoadingScreen();
-
+    }
 /**
  * Versión de la aplicación.
  * @constant {string}
  */
 const VERSION = '4.2.0';
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => 
+    {
     // Registra la versión de la aplicación en la consola.
     console.log(`Aplicación Mizulegends iniciada. Versión: ${VERSION}`);
-
-});
+    }
+);
